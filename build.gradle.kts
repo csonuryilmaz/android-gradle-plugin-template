42b6ca73f9432d93959258e05eed048c5dd05c28
Onur Yılmaz <csonuryilmaz@gmail.com>
2023-09-07 07:28:54 +0300

    Add detekt plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.jvm) apply false
+    id("io.gitlab.arturbosch.detekt") version "1.23.0"
 }
 
 subprojects {
